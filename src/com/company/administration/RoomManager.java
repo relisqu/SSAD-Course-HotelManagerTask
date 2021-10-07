dc78228282b7fd5e5b3165b557fb25848c7cb30f
KarinaTyulebaeva <78172633+KarinaTyulebaeva@users.noreply.github.com>
2021-10-07 13:14:22 +0300

    исправила ошибки

--- src/com/company/administration/RoomManager.java
+++ src/com/company/administration/RoomManager.java
@@ -21,18 +21,20 @@ public class RoomManager {
      * and suitability by attributes.
      * If all parameters are met, the room is reserved.
      * It does not return anything, it only changes the parameters of a specific room.
-     * @param access -
-     * @param client -
+     * @param client - human, who want to book the room
      * @param type - type of the room for booking
      * @param peopleCount - number of people for booking
      */
     public void bookRoom(RoomAccess access, Human client,
                          RoomType type, int peopleCount) {
+        Room currentRoom;
      for(int i=0; i<access.getRooms().size(); i++){
-        if(access.getRooms().get(i).isOccupied()) continue;
+         currentRoom = access.getRooms().get(i);
+        if(currentRoom.isOccupied()) continue;
         else{
-            if(access.getRooms().get(i).getSize() >= peopleCount && access.getRooms().get(i).getType()==type){
-                access.getRooms().get(i).CheckIn(client, peopleCount);
+            if(currentRoom.getSize() >= peopleCount && currentRoom.getType()==type){
+                currentRoom.CheckIn(client, peopleCount);
+                break;
             }
         }
      }
@@ -41,10 +43,9 @@ public class RoomManager {
     /**
      * This method allows us to remove the reservation from the room.
      * It does not return anything, it only changes the parameters of a specific room.
-     * @param access -
      * @param room - the room we want to free
      */
     public void unbookRoom(RoomAccess access, Room room){
-        access.getRooms().get(room.getNumber()).CheckOut();
+        room.CheckOut();
     }
 }
