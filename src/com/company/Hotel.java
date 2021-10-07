643bd7e959cdf0c2a865c9c9803341bf67c8245d
DomrachevIvan <domrachev03@mail.ru>
2021-10-07 16:34:40 +0300

    Added realisation of hotel class

--- src/com/company/Hotel.java
+++ src/com/company/Hotel.java
@@ -1,48 +1,103 @@
 package com.company;
 
 import com.company.room.Room;
-import com.company.room.RoomType;
 import com.company.staff.Staff;
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class Hotel implements RoomAccess, StaffAccess {
     private ArrayList<Room> rooms;
     private ArrayList<Staff> staff;
-
     public Hotel(ArrayList<Room> rooms) {
+        this.rooms = rooms;
     }
 
     public ArrayList<Staff> getStaff() {
         return staff;
     }
 
+    private ListIterator<Staff> findStaff (Staff searchRequest) {
+        ListIterator<Staff> curStaff = staff.listIterator();
+
+        while(curStaff.hasNext()) {
+            if(curStaff.next().getPersonalInformation().getId() == searchRequest.getPersonalInformation().getId()) {
+                curStaff.previous();
+                break;
+            }
+        }
+
+        return curStaff;
+    }
+
+    private ListIterator<Room> findRoom (Room searchRequest) {
+        ListIterator<Room> curRoom = rooms.listIterator();
+
+        while(curRoom.hasNext()) {
+            if(curRoom.next().getNumber() == searchRequest.getNumber()) {
+                curRoom.previous();
+                break;
+            }
+        }
+
+        return curRoom;
+    }
+
     @Override
     public void updateStaff(Staff... updatedStaff) {
+        for(Staff staffForUpdate: updatedStaff) {
+            ListIterator<Staff> it = findStaff(staffForUpdate);
+
+            if(it.hasNext()) {
+                it.set(staffForUpdate);
+            }
+        }
+    }
+
+    @Override
+    public void deleteStaff(Staff... deletedStaff) {
+        for(Staff staffToDelete : deletedStaff) {
+            ListIterator<Staff> it = findStaff(staffToDelete);
+
+            if(it.hasNext()) {
+                it.remove();
+            }
+        }
+    }
+
+    @Override
+    public void createStaff(Staff... createdStaff) {
+        for(Staff newStaff : createdStaff) {
+            if(!findStaff(newStaff).hasNext()) {
+                staff.add(newStaff);
+            }
+        }
 
     }
 
     @Override
-    public void deleteStaff(Staff... firedStaff) {
-
+    public ArrayList<Room> getRooms() {
+        return rooms;
     }
 
     @Override
-    public void createStaff(Staff... createStaff) {
+    public void updateRooms(Room... updatedRooms) {
+        for(Room roomForUpdate: updatedRooms) {
+            ListIterator<Room> it = findRoom(roomForUpdate);
 
-    }
-
-    @Override
-    public ArrayList<Room> getRooms(Human client, RoomType type, int peopleCount) {
-        return null;
-    }
-
-    @Override
-    public void updateRooms(Room... newRooms) {
-
+            if(it.hasNext()) {
+                it.set(roomForUpdate);
+            }
+        }
     }
 
     @Override
     public void deleteRooms(Room... deletedRooms) {
+        for(Room roomToDelete : deletedRooms) {
+            ListIterator<Room> it = findRoom(roomToDelete);
 
+            if(it.hasNext()) {
+                it.remove();
+            }
+        }
     }
 }
