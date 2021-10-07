eec3bc12a1ab112b909744b6b79ab22fa964612d
Igor Alentev <i.alentev@innopolis.university>
2021-10-07 15:12:48 +0300

    Fix unbooking and reformat RoomManager

--- src/com/company/administration/RoomManager.java
+++ src/com/company/administration/RoomManager.java
@@ -13,7 +13,8 @@ import java.sql.Time;
  * and their availability.
  */
 public class RoomManager {
-    public RoomManager() {}
+    public RoomManager() {
+    }
 
     /**
      * This method allows us to book a room.
@@ -23,41 +24,35 @@ public class RoomManager {
      * and suitability by attributes.
      * If all parameters are met, the room is reserved.
      * It does not return anything, it only changes the parameters of a specific room.
+     *
      * @param access      hotel rooms information
-     * @param client - human, who want to book the room
-     * @param type - type of the room for booking
-     * @param peopleCount - number of people for booking
+     * @param client      human, who want to book the room
+     * @param type        type of the room for booking
+     * @param peopleCount number of people for booking
      */
     public void bookRoom(RoomAccess access, Human client,
                          RoomType type, int peopleCount) {
         Room currentRoom;
-     for(int i=0; i<access.getRooms().size(); i++){
-         currentRoom = access.getRooms().get(i);
-        if(currentRoom.isOccupied()) continue;
-        else{
-            if(currentRoom.getSize() >= peopleCount && currentRoom.getType()==type){
-                currentRoom.CheckIn(client, peopleCount);
-                break;
+        for (int i = 0; i < access.getRooms().size(); i++) {
+            currentRoom = access.getRooms().get(i);
+            if (currentRoom.isOccupied()) continue;
+            else {
+                if (currentRoom.getSize() >= peopleCount && currentRoom.getType() == type) {
+                    currentRoom.checkIn(client, peopleCount);
+                    break;
+                }
             }
         }
-     }
-    }
-
-    /**
-     * This method allows us to remove the reservation from the room.
-     * It does not return anything, it only changes the parameters of a specific room.
-     * @param room - the room we want to free
-     */
-    public void unbookRoom(RoomAccess access, Room room){
-        room.CheckOut();
     }
 
     /**
      * Try to free room from a client.
      * E.g. departure time has come or
      * the client decided to leave himself.
+     *
      * @param access hotel rooms information
      * @param room   room to be freed
      */
-    public void unbookRoom(RoomAccess access, Room room){}
+    public void unbookRoom(RoomAccess access, Room room) {
+    }
 }
