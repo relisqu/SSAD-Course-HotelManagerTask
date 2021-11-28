1355ae8a34ad876f368e035134a1ec5a2398937a
Igor <alentev.olm@ya.ru>
2021-11-28 19:56:42 +0300

    Document methods implementations

--- src/com/company/staff/SecurityGuard.java
+++ src/com/company/staff/SecurityGuard.java
@@ -10,16 +10,25 @@ public class SecurityGuard extends Staff {
         super(personalInformation, type);
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     void comeToRoom(int roomNumber) {
         System.out.println("I am SecurityGuard and I came to room "+roomNumber);
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     void communicateWithClient() {
         System.out.println("Hey man, what kind of protection would you need?");
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     boolean tryProvideService() {
         double isAbleToProvideService = (Math.random()+1)%100;
@@ -31,11 +40,17 @@ public class SecurityGuard extends Staff {
         return true;
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     void manageProvideServiceError() {
         System.out.println("Let me assign you to my friend, great psychologist");
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     void requestPayment() {
         System.out.println("Gimme 7k, I will protect you from firemen");
