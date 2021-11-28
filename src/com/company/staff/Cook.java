a662e08855a17623bfde1f0c80e4d0796b9073af
Igor <alentev.olm@ya.ru>
2021-11-28 19:54:33 +0300

    Document methods implementation

--- src/com/company/staff/Cook.java
+++ src/com/company/staff/Cook.java
@@ -10,16 +10,25 @@ public class Cook extends Staff {
         super(personalInformation, type);
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     void comeToRoom(int roomNumber) {
         System.out.println("I am Cook and I came to room "+roomNumber);
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     void communicateWithClient() {
         System.out.println("Do you want salad with garlic?");
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     boolean tryProvideService() {
         double isAbleToProvideService = (Math.random()+5)%100;
@@ -31,11 +40,17 @@ public class Cook extends Staff {
         return true;
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     void manageProvideServiceError() {
         System.out.println("I will redo the salad, my apologises.");
     }
 
+    /**
+    * Templated in Staff service algorithm step implementation.
+    */
     @Override
     void requestPayment() {
         System.out.println("No need to pay. Just come to stolovka");
